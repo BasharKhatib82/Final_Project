09cb59b11a83353d8009b2fc64ca538ee5eb3366
BasharKhatib82 <technion.bashar@gmail.com>
2025-08-19 08:01:49 +0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,16 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://www.respondify-crm.co.il");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
+
+
+
 // ✅ ראוטים
 app.use("/admin", adminRoutes);
 app.use("/auth", authRoutes);
