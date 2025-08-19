63168a334562c1db4ad5a7c1fd2cc3d2d7a6d006
BasharKhatib82 <technion.bashar@gmail.com>
2025-08-19 04:19:26 +0300

    replace Environment Variables

--- backend/utils/dbSingleton.js
+++ backend/utils/dbSingleton.js
@@ -7,22 +7,22 @@ const dbSingleton = {
   getConnection: () => {
     if (!connection) {
       connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "leads_management",
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
       });
 
       connection.connect((err) => {
         if (err) {
-          console.error("Error connecting to database:", err);
+          console.error("❌ Error connecting to database:", err);
           throw err;
         }
-        console.log("Connected to MySQL!");
+        console.log("✅ Connected to MySQL!");
       });
 
       connection.on("error", (err) => {
-        console.error("Database connection error:", err);
+        console.error("⚠️ Database connection error:", err);
         if (err.code === "PROTOCOL_CONNECTION_LOST") {
           connection = null;
         }
