4952580ba1d3a6b0a688ba43aa8bdcf355e67b32
BasharKhatib82 <technion.bashar@gmail.com>
2025-08-25 17:47:18 +0300

    fix

--- frontend/src/components/Pages/Contact.jsx
+++ frontend/src/components/Pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Popup from "./Tools/Popup";
+import Popup from "../Tools/Popup.jsx";
 
 const api = process.env.REACT_APP_API_URL;
 
