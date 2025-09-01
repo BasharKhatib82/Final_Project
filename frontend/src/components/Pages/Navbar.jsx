d317eccfd432abae6dae9d8906fd32142498542e
BasharKhatib82 <technion.bashar@gmail.com>
2025-09-01 23:22:08 +0300

    fix

--- frontend/src/components/Pages/Navbar.jsx
+++ frontend/src/components/Pages/Navbar.jsx
@@ -94,8 +94,8 @@ function Navbar() {
             התנתקות
           </button>
         ) : (
-          <NavLink
-            to="/userlogin"
+          <button
+            onClick={navigate("/userlogin")}
             className="flex items-center gap-2 font-rubik text-white bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg transition"
           >
             <Icon
@@ -105,7 +105,7 @@ function Navbar() {
               color="white"
             />
             התחברות
-          </NavLink>
+          </button>
         )}
       </div>
     </nav>
