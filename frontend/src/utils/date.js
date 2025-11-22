22d040d840f27c11bae4c6ce3dc1323c9f1c2fb1
BasharKhatib82 <technion.bashar@gmail.com>
2025-11-22 23:37:26 +0200

    fix

--- frontend/src/utils/date.js
+++ frontend/src/utils/date.js
@@ -1,8 +1,8 @@
 // frontend/src/utils/date.js
 
 import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
