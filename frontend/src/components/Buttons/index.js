c73db6820222f4cb8823f3c20cbd5b9418c4e874
BasharKhatib82 <technion.bashar@gmail.com>
2026-01-08 17:41:52 +0200

    fix

--- frontend/src/components/Buttons/index.js
+++ frontend/src/components/Buttons/index.js
@@ -3,3 +3,4 @@ export { default as AddSaveButton } from "./AddSaveButton";
 export { default as DeleteButton } from "./DeleteButton";
 export { default as ExitButton } from "./ExitButton";
 export { default as NavigationButton } from "./NavigationButton";
+export { default as AppButton } from "./AppButton";
